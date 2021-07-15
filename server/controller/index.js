52af13e6619e04f43787704acf5afaa8f9b1376e
Jacky Wu <Jackyw1@uci.edu>
2021-07-15 01:59:21 -0700

    add stuff

--- server/controller/index.js
+++ server/controller/index.js
@@ -11,6 +11,12 @@ module.exports = {
     });
   },
 
+  // req.query example: /test/:id/otherTest?key=value&myOtherKey=myOtherValue
+  // {
+  //   key: value,
+  //   myOtherKey: myotherValue,
+  // }
+
   getSomethingPost(req, res) {
     console.log(req.query.myKey);
     console.log(req.params.id);
